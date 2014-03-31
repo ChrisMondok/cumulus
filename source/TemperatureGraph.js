0f3a1794d7d9e3fd37d5b253d7b7d1bfbf25f5f4
Chris Mondok <chris.mondok@gmail.com>
2014-03-30 21:10:44 -0400

    Show freezing on the temperature graph

--- source/TemperatureGraph.js
+++ source/TemperatureGraph.js
@@ -32,13 +32,26 @@ enyo.kind({
 		this.inherited(arguments);
 
 		var step = this.getStep(),
-			amount = animStep;
+			amount = animStep,
+			min = this.get('min'),
+			max = this.get('max');
 
 		for(var i = this.min; i < this.max; i+=step) {
 			var y = this.valueToY(i);
 			this._ctx.beginPath();
 			this._ctx.moveTo(0,this.valueToY(i));
 			this._ctx.lineTo(this.$.canvas.getBounds().width * amount,this.valueToY(i));
+			this._ctx.stroke();
+			this._ctx.closePath();
+		}
+
+		if(min <= 32 && max >= 32)
+		{
+			this._ctx.strokeStyle = "#9999FF";
+			this._ctx.beginPath();
+			var f = this.valueToY(32);
+			this._ctx.moveTo(0, f);
+			this._ctx.lineTo(this.$.canvas.getBounds().width * amount, f);
 			this._ctx.stroke();
 			this._ctx.closePath();
 		}
